cdfac8cbf1c9467452db19ae135eab89f37273f4
supersiem <vankeulensiem@gmail.com>
2024-09-02 10:14:20 +0200

    update mysql setup

--- setup.bash
+++ setup.bash
@@ -3,8 +3,7 @@
 sudo apt update
 sudo apt install mysql-server
 sudo mysql_secure_installation utility
-sudo mysql --user=root --password=  -e "source setup.sql"
-sudo apt install python3
+sudo mysql --user=root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'siemsiem'; FLUSH PRIVILEGES;"sudo apt install python3
 sudo apt install python3-pip
 sudo apt install python3-venv
 sudo python3 -m venv 050guessr_server/venv
