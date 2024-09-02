c83580b0aa9a58f9a0acc30534760d85442f76b3
supersiem <vankeulensiem@gmail.com>
2024-09-02 10:07:18 +0200

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -3,12 +3,12 @@
 sudo apt update
 sudo apt install mysql-server
 sudo mysql_secure_installation utility
-sudo mysql --user=root --password=xxxxxx  -e "source setup.sql"
+sudo mysql --user=root --password=  -e "source setup.sql"
 sudo apt install python3
 sudo apt install python3-pip
 sudo apt install python3-venv
 sudo python3 -m venv 050guessr_server/venv
 source 050guessr_server/venv/bin/activate
-sudo pip install -r 050guessr_server/requirements.txt
+sudo pip install -r 050guessr_server/requirements.txt --break-system-packages
 sudo nano 050guessr_server/keys_.py
 python3 050guessr_server/setup.py
