7fca73b7edc86a4ce39e3695e6007ad14b568253
supersiem <vankeulensiem@gmail.com>
2024-09-03 10:41:10 +0200

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -10,5 +10,5 @@ sudo apt install python3-venv
 sudo python3 -m venv 050guessr_server/venv
 source 050guessr_server/venv/bin/activate
 pip install -r 050guessr_server/requirements.txt 
-sudo nano 050guessr_server/keys_.py
+#sudo nano 050guessr_server/keys_.py
 python3 050guessr_server/setup.py
