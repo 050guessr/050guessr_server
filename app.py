d4d5d2054cec0f6845e3a873b6f21f0833575dcd
supersiem <vankeulensiem@gmail.com>
2025-03-07 21:40:33 +0100

    kuctxdjtxdetxhdthxdt

--- app.py
+++ app.py
@@ -19,11 +19,14 @@ CORS(app)
 pastalock = False
 database = sqlite_commands(database_name="main.db")
 
+
 def encode_to_base64(input_string):
     return base64.b64encode(input_string.encode("utf-8"))
 
+
 def decode_from_base64(input_string):
     return base64.b64decode(input_string).decode("utf-8")
+
 
 def set_database(naam):
     global pastalock
@@ -31,6 +34,7 @@ def set_database(naam):
         return "locked"
     database.set_database(naam)
     return "gezet"
+
 
 def create_default_table(naam):
     database.create_table(
@@ -50,6 +54,7 @@ def create_default_table(naam):
     print(f"Table '{naam}' created successfully!")
     return "created"
 
+
 @app.route("/maak_acount_V2/<username>/<password>/<email>")
 def maak_acount_V2(username, password, email):
 
@@ -67,10 +72,11 @@ def maak_acount_V2(username, password, email):
                 "email": email,
                 "user_score": 0,
                 "user_rank": 0,
-                "user_key": str(random.randint(1000, 9999)*int(time.time())/50 ),
+                "user_key": str(random.randint(1000, 9999) * int(time.time()) / 50),
             },
         )
         return "account aangemaakt"
+
 
 @app.route("/login/<username>/<password>")
 def login(username, password):
@@ -84,9 +90,11 @@ def login(username, password):
     else:
         return "gebruiker bestaat niet"
 
+
 @app.route("/get_score/<username>")
 def get_score(username):
     return str(database.get_item("users", "username", username)[4])
+
 
 @app.route("/leaderboard")
 def get_leaderboard():
@@ -121,7 +129,9 @@ def get_leaderboard_moeilijk():
     Returns:
         json: A JSON response containing the sorted leaderboard without user_key and password fields.
     """
-    leaderboard = database.get_all_items_sorted("users", "user_score_moeilijk", descending=True)
+    leaderboard = database.get_all_items_sorted(
+        "users", "user_score_moeilijk", descending=True
+    )
 
     # Exclude 'user_key' and 'password' fields from the leaderboard data
     filtered_leaderboard = [
@@ -135,18 +145,26 @@ def get_leaderboard_moeilijk():
 
     return flask.jsonify(filtered_leaderboard)
 
+
 def set_score(key, score, pasta, waar):
-    try:score = int(score)
-    except ValueError:return "Ongeldige score, moet een getal zijn.", 400  # Bad Request
-    if score > 10000:return "ik ben zoo boos ike ga aleen naar de speeltuin", 400
-    if not beveiliging.ontsleutel_en_vergelijken(pasta, str(score)):return "ik ben zoo boos ike ga aleen naar de speeltuin", 400
+    try:
+        score = int(score)
+    except ValueError:
+        return "Ongeldige score, moet een getal zijn.", 400  # Bad Request
+    if score > 10000:
+        verban(key)
+        return "ik ben zoo boos ike ga aleen naar de speeltuin", 400
+    if not beveiliging.ontsleutel_en_vergelijken(pasta, str(score)):
+        return "ik ben zoo boos ike ga aleen naar de speeltuin", 400
     oude_score = database.get_item("users", "user_key", key)[4]
     database.edit_item("users", waar, score, "user_key", key)
     return str(oude_score)
 
+
 @app.route("/set_score/<key>/<score>/<pasta>")
 def set_score_eenvoudig(key, score, pasta):
     return set_score(key, score, pasta, "user_score")
+
 
 @app.route("/set_score_moeilijk/<key>/<score>/<pasta>")
 def set_score_moeilijk(key, score, pasta):
@@ -162,7 +180,7 @@ def verban(key):
     if keys.auto_ban:
         database.edit_item("users", "user_score", 0, "user_key", str(key))
         database.edit_item("users", "password", "verbanen", "user_key", str(key))
-        database.edit_item("users", "user_key", "verbannen", "user_key", str(key)) 
+        database.edit_item("users", "user_key", "verbannen", "user_key", str(key))
     if keys.auto_flag:
         database.edit_item("users", "user_rank", 1, "user_key", str(key))
 
@@ -174,8 +192,10 @@ def restore_acount(email):
     length = 50
     data = database.get_item("users", "email", email)
     if data:
-        letters = string.ascii_letters + string.digits  # No punctuation, just letters and digits
-        random_string = ''.join(random.choice(letters) for i in range(length))
+        letters = (
+            string.ascii_letters + string.digits
+        )  # No punctuation, just letters and digits
+        random_string = "".join(random.choice(letters) for i in range(length))
         database.edit_item(
             "users",
             "password",
@@ -186,6 +206,7 @@ def restore_acount(email):
 
         mail.send_mail(email, random_string)
     return email
+
 
 @app.route("/set_password/<old_password>/<new_password>/<key>")
 def set_password(old_password, new_password, key):
@@ -202,7 +223,8 @@ def set_password(old_password, new_password, key):
         return "password updated"
     else:
         return "wrong password"
-    
+
+
 @app.route("/set_bage/<key>/<bage_num>")
 def set_bage(key, bage_num):
     database.edit_item(
@@ -213,6 +235,7 @@ def set_bage(key, bage_num):
         key,
     )
     return "bage updated"
+
 
 @app.route("/ADMIN/<admin_key>/update/<user_key>/<type>/<new_value>")
 def admin_update(admin_key, user_key, type, new_value):
@@ -227,7 +250,8 @@ def admin_update(admin_key, user_key, type, new_value):
         return "updated"
     else:
         return "incorecte ADMIN key"
-    
+
+
 @app.route("/ADMIN/<admin_key>/delete/<user_key>")
 def admin_delete(admin_key, user_key):
     if admin_key == keys.admin_key:
@@ -238,6 +262,7 @@ def admin_delete(admin_key, user_key):
     else:
         return "incorecte ADMIN key"
 
+
 @app.route("/ADMIN/<admin_key>/unfilterd_list")
 def admin_unfilterd_list(admin_key):
     if admin_key == keys.admin_key:
@@ -245,13 +270,14 @@ def admin_unfilterd_list(admin_key):
     else:
         return "incorecte ADMIN key"
 
+
 @app.errorhandler(500)
 def internal_error(error):
     # Log the error (optional)
     app.logger.error(f"Server Error: {error}")
-    
+
     # Optionally, trigger an external command to restart the app
-    os.system('pm2 restart app')  # Restarts using pm2
+    os.system("pm2 restart app")  # Restarts using pm2
 
     return "Internal Server Error", 500
 
@@ -263,6 +289,6 @@ except:
     create_default_table("users")
 
 from waitress import serve
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5000)
 
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=5000, url_scheme="https")
